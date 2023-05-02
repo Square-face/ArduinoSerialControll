a192e6e5e4a6ede44ed73e777339f8dd8ccf71f3
Square-Face <sq8.square.face@gmail.com>
2023-05-02 13:28:03 +0200

    added tests and shit

--- local/main.py
+++ local/main.py
@@ -1,2 +1,13 @@
-import serial, time
+import yaml, Arduino
 
+def read_config(config_path):
+    """Read configuration file"""
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+if __name__ == "__main__":
+
+    config = read_config('config.yaml')
+    Arduino.Arduino(config["port"], config["baudrate"])
